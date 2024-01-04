ce732213d817bceb6c334b6690a1f804d5de1829
Frédérique Mittelstaedt <git@gfm.io>
2024-01-04 15:07:41 +0000

    scaffold project

---
+++ examples/basic/src/index.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+
+describe("This example test", () => {
+    it("should run", () => {
+        expect(true).to.equal(true);
+    });
+});
